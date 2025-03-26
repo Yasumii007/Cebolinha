4ea05a2067ead107b502ab2e964fa8870c953670
Yasumii007 <yasmimalvesrodrigues16@gmail.com>
2025-03-26 20:44:23 -0300

    Atividade cebolinha, o usuario informa uma frase e ela e ajeitada para maior similaridade com a fala do cebolinha

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string frase;
+
+Console.WriteLine("Ola, prazer sou o cebolinha, por favor me fale frase que você deseja que eu fale");
+
+frase = Console.ReadLine()!;
+
+Console.WriteLine($"A frase escolhida por você foi:{frase} ");
+
+frase = frase.Replace("R", "L").Replace("r", "l");
+
+Console.WriteLine($"Quando eu falo a frase fica assim: {frase}");
